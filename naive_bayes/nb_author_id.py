0a2bc67502491eb5c4cb45b56f536f856d3407db
mjlittle <mary-jane.little@airbus.com>
2018-03-01 14:40:41 +0000

    Update nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,10 +26,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
-labels = np.array([Sara],[Chris])
-
-
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred, labels_test)
+labels = np.array([labels_train],[labels_test])
+features = np.array([features_train],[features_test])
+print accuracy_score(pred, labels_test)
 #########################################################
 
 
